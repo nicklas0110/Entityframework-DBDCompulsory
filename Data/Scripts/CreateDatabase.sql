5741b0091d7ab3a8fb5f0d61282539adaecc1998
Nick <nicklas.nick@live.dk>
2024-03-04 13:03:55 +0100

    added categories

--- Data/Scripts/CreateDatabase.sql
+++ Data/Scripts/CreateDatabase.sql
@@ -10,3 +10,16 @@ CREATE TABLE Products (
     Price DECIMAL(10, 2)
 );
 GO
+
+CREATE TABLE Categories (
+                            CategoryId INT PRIMARY KEY,
+                            CategoryName VARCHAR(255)
+);
+GO
+
+ALTER TABLE Products
+    ADD category_id INT;
+
+ALTER TABLE Products
+    ADD CONSTRAINT FK_Products_Categories FOREIGN KEY (category_id) REFERENCES Categories(CategoryId);
+GO
